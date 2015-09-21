908afd1209da88e5b75412d0038b4790988636eb
NamDongHyun <NamDongHyun@NamThanPC>
2015-09-21 12:41:40 +0900

    ㅁㄴㅇㄹ

---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,26 @@
+package org.dimigo.exception;
+
+public class MovieTest {
+	public static void main(String[] args) {
+		try {
+			Movie[] movies = {
+				new Movie("앤트맨", 12), new Movie("사도", 12), new Movie("베테랑", 15)
+			};
+			
+			int age = 13;
+			
+			for(Movie movie : movies) {
+				buyTicket(movie, age); // 여기서 예외 던짐
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private static void buyTicket(Movie movie, int age) throws AgeCheckException {
+		if (age < movie.getLimitAge()){
+			throw new AgeCheckException(movie);
+		}
+		System.out.println(movie.getTitle() + " 영화 즐감하세용~~");
+	}
+}
