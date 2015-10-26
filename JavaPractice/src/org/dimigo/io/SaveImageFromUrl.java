dc85c44ac5d657b54fbfa8afaa4e566aaea7aeea
NamDongHyun <NamDongHyun@NamThanPC>
2015-10-26 12:50:17 +0900

    asdf

---
+++ JavaPractice/src/org/dimigo/io/SaveImageFromUrl.java
@@ -0,0 +1,22 @@
+package org.dimigo.io;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SaveImageFromUrl {
+	public static void main(String[] args) {
+		String imageurl = "http://wonjuni.co.kr/home/wp-content/themes/dt-the7/inc/presets/images/full/skin1.header-logo-regular.png";
+		try {
+			URL url = new URL(imageurl);
+			
+			try(InputStream is = url.openStream();
+					OutputStream os = new FileOutputStream("files/logo.png"))
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
